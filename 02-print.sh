ce71edcd1d2dcf1e06952bbe619181021fad8f1b
gauravkr04 <gauravaneja1983@gmail.com>
2022-01-22 19:50:24 +0530

    Print the messages in Color - Bold and Underline - comments updated

--- 02-print.sh
+++ 02-print.sh
@@ -23,6 +23,10 @@ echo Hello World!!
 echo -e "\e[31mRED\e[32mGREEN\e[33mYELLOW\e[34mBLUE\e[35mMAGENTA\e[36mCYAN\e[0m"
 
 # The same color can be print with more Brightness and BOLD color
+# 1 for BOLD
+# 4 for Underline
+# 2 for less brightness
+# 3 for normal
 echo -e "\e[1;31mRED\e[1;32mGREEN\e[1;33mYELLOW\e[1;34mBLUE\e[1;35mMAGENTA\e[1;36mCYAN\e[0m"
 echo -e "\e[4;31mRED\e[4;32mGREEN\e[4;33mYELLOW\e[4;34mBLUE\e[4;35mMAGENTA\e[4;36mCYAN\e[0m"
 echo -e "\e[2;31mRED\e[2;32mGREEN\e[2;33mYELLOW\e[2;34mBLUE\e[2;35mMAGENTA\e[2;36mCYAN\e[0m"
