059fecce7bafe1f77f46211945592fd0b2875a0d
gauravkr04 <gauravaneja1983@gmail.com>
2022-01-22 19:29:45 +0530

    Added comments for She-Bang Interpreter

--- 01-sample.sh
+++ 01-sample.sh
@@ -1,4 +1,6 @@
 #! /bin/bash
-# It denotes the path of interpreter of the shell
+# It denotes the path of interpreter of the shell and ensures that rest of the lines executes with this interpreter
+# For a file there is only one She-Bang possible
+# Also, She-Bang needs to be in the first line.
 
 ls
