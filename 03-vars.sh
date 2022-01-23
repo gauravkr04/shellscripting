5c15a2ad4a3cf81104211ee379fce8465067f8b2
gauravkr04 <gauravaneja1983@gmail.com>
2022-01-23 11:36:30 +0530

    Variable Assignment

---
+++ 03-vars.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+# Assignment of name to set of Data that is called Variable
+# in Bash, declaration : VAR = DATA
+# in Bash, accessing the variable : $VAR or  ${VAR}
+# The Variable can be hardcoded or can be assigned a value dynamically.
+
+student_name="Gaurav"
+
+echo student_name = $student_name
+echo student_name = ${student_name}
+
