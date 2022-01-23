f12b49f9e76ee07cf3d63435c63bfd47598f6796
gauravkr04 <gauravaneja1983@gmail.com>
2022-01-23 12:39:01 +0530

    Arithmatic Expression

--- 03-vars.sh
+++ 03-vars.sh
@@ -22,5 +22,5 @@ DATE=$(date +%F)
 echo Good Morning, Today date is $DATE
 
 # Arithmatic Substitution
-VAR1 = $((2+3+4-1/6))
+VAR1 = $((2+3+4-1/6-1))
 echo The output of Arithmatic expression is $VAR1
