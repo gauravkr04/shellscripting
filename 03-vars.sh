083af8bd56857e9aa01f6cdf56f78ed651316700
gauravkr04 <gauravaneja1983@gmail.com>
2022-01-23 11:54:56 +0530

    Hardcode vs Dynamic Assignment

--- 03-vars.sh
+++ 03-vars.sh
@@ -3,10 +3,20 @@
 # Assignment of name to set of Data that is called Variable
 # in Bash, declaration : VAR = DATA
 # in Bash, accessing the variable : $VAR or  ${VAR}
-# The Variable can be hardcoded or can be assigned a value dynamically.
+
 
 student_name="Gaurav"
 
 echo student_name = $student_name
 echo student_name = ${student_name}
 
+# The Variable can be hardcoded or can be assigned a value dynamically.
+# We need to put the output message in quotes in case it has spaces
+
+# Hard Coded Assignment
+DATE = 2022-01-23
+echo "Good Morning, Today's date is $DATE"
+
+#Command Substitution
+DATE = $(date +%F)
+echo "Good Morning, Today's date is $DATE"
