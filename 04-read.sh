a92f40118d24bf99ee543da2d66f9ae96ef37766
gauravkr04 <gauravaneja1983@gmail.com>
2022-01-25 20:24:10 +0530

    Read Command - Before Execution

---
+++ 04-read.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Input in shell scripting can be taken in two ways :
+# 1. During Execution  2. Before Execution
+read -p "Enter Your Name : " name
+echo " Your Name = $name "
+read -p "Enter Course Name : " course
+echo "Your Course = $course"
