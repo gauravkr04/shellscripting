28a4235a1cddd40073699afef3c7208114f9b3e8
gauravkr04 <gauravaneja1983@gmail.com>
2022-01-25 20:27:49 +0530

    Comments Updated

--- 04-read.sh
+++ 04-read.sh
@@ -2,6 +2,9 @@
 
 # Input in shell scripting can be taken in two ways :
 # 1. During Execution  2. Before Execution
+# During Execution inputs can only be done when the execution is manual. Will not work for Automation.
+#
+
 read -p "Enter Your Name : " name
 echo " Your Name = $name "
 read -p "Enter Course Name : " course
