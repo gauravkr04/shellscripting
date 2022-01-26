67b9cd42b7ed9a40cc22ac0cbbd69129470d3ee1
gauravkr04 <gauravaneja1983@gmail.com>
2022-01-26 17:21:20 +0530

    roboshop file updated to handle invalid input and custom message

--- roboshop-project/roboshop.sh
+++ roboshop-project/roboshop.sh
@@ -3,4 +3,11 @@
 # Individual scripts for each modules
 # Individual scripts for all components wrapped in MakeFile
 
-bash components/$1.sh
+if [ -f components/$1.sh ]; then
+  bash components/$1.sh
+else
+  echo -e "\e[1;31mInvalid Input\e[0m"
+  echo -e "\e[1;33mValid inputs are -
+            frontend|mongodb|catalogue|redis|user|cart|mysql|shipping|payment|rabbitmq|dispatch\e[0m"
+fi
+
