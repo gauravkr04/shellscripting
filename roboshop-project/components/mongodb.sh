73807e90117c385153b27c0ffce5087f3ef7f3d0
gauravkr04 <gauravaneja1983@gmail.com>
2022-01-27 16:03:12 +0530

    Mongodb file - Download the schema and Load it

--- roboshop-project/components/mongodb.sh
+++ roboshop-project/components/mongodb.sh
@@ -1,4 +1,4 @@
-echo Installing mongodb
+#cho Installing mongodb
 
 #1. Setup MongoDB repos.
 #
@@ -25,12 +25,21 @@ echo Installing mongodb
 ## systemctl restart mongod
 #
 #```
+# Download the schema and load it
+# curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip"
+
+# cd /tmp
+# unzip mongodb.zip
+# cd mongodb-main
+# mongo < catalogue.js
+# mongo < users.js
+
 
 #Importing the common script
 source components/common.sh
 
 #1. Setup MongoDB repos.
-echo "Installing MongoDB repo file"
+echo "Downloading MongoDB repo file"
 curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo &>>$LOG_FILE
 
 # Install Mongo & Start Service.
@@ -46,3 +55,16 @@ sed -i -e 's/127.0.0.1/0.0.0.0' /etc/mongod.conf &>>$LOG_FILE
 echo "Start Database"
 systemctl enable mongod &>>$LOG_FILE
 systemctl start mongod &>>$LOG_FILE
+
+# Download the schema and load it
+echo "Download Schema"
+curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip" &>>$LOG_FILE
+
+echo "Extract Schema"
+cd /tmp
+unzip -o mongodb.zip &>>$LOG_FILE
+
+echo "Load Schema"
+cd mongodb-main &>>$LOG_FILE
+mongo < catalogue.js &>>$LOG_FILE
+mongo < users.js &>>$LOG_FILE
