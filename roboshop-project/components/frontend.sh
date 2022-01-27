d3384f1115bc7bf247ae1a62778449e5d6716de6
gauravkr04 <gauravaneja1983@gmail.com>
2022-01-27 07:38:53 +0530

    frontend component - redirecting and appending output in a log file

--- roboshop-project/components/frontend.sh
+++ roboshop-project/components/frontend.sh
@@ -38,4 +38,7 @@ echo "Installing frontend"
 
 
 echo "Installing NGINX"
-yum install nginx -y >/tmp/roboshop.log
+yum install nginx -y >>/tmp/roboshop.log
+
+echo "Download frontend Content"
+curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" >>/tmp.roboshop.log
