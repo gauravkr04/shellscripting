6583d1da6b05072ebc332f2be8dd92976a822841
gauravkr04 <gauravaneja1983@gmail.com>
2022-01-27 08:25:15 +0530

    Completed the setup with unzip the content and copy the file to right location. Enable and start NGINX

--- roboshop-project/components/frontend.sh
+++ roboshop-project/components/frontend.sh
@@ -48,3 +48,22 @@ echo "Download frontend Content"
 curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>>$LOG_FILE
 
 #cd /usr/share/nginx/html
+echo "Clean Old Content"
+rm -rf /usr/share/nginx/html/* &>>$LOG_FILE
+
+echo "Extract Frontend Content"
+cd /tmp
+unzip /tmp/frontend.zip &>>$LOG_FILE
+
+echo "Copy Extracted content to Nginx path"
+cp -r frontend-main/static/* /usr/share/nginx/html/ &>>$LOG_FILE
+
+echo "Copy Nginx Roboshop Config"
+cp frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf &>>$LOG_FILE
+
+echo "Start Nginx Service"
+systemctl enable nginx &>>$LOG_FILE
+systemctl start nginx &>>$LOG_FILE
+
+
+
