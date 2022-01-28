40e5166f4a9489380de6d56adf9120940523bdb0
gauravkr04 <gauravaneja1983@gmail.com>
2022-01-29 04:27:05 +0530

    Creating ec2 instance - creating Instance - passing instance name as Argument

--- roboshop-project/create-ec2.sh
+++ roboshop-project/create-ec2.sh
@@ -10,7 +10,7 @@
 INSTANCE_NAME=$1
 
 if [ -z $INSTANCE_NAME ]; then
-  echo -e "\e[1,31mThe Instance Name has not been passed\e[0m"
+  echo -e "\e[1;31mThe Instance Name has not been passed\e[0m"
   exit
 fi
 
@@ -18,7 +18,7 @@ fi
 AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-7-DevOps-Practice" --query 'Images[*].[ImageId]' --output text)
 
 if [ -z $AMI_ID ]; then
-  echo -e "\e[1,31mUnable to find the Image AMI_ID\e[0m"
+  echo -e "\e[1;31mUnable to find the Image AMI_ID\e[0m"
   exit
 else
   echo $AMI_ID
