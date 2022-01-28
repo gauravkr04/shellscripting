684726be6ff3f915044244bc89d82c0f9f0b3fec
gauravkr04 <gauravaneja1983@gmail.com>
2022-01-29 04:49:28 +0530

    Creating ec2 instance - creating Instance - check if instance is already running

--- roboshop-project/create-ec2.sh
+++ roboshop-project/create-ec2.sh
@@ -9,7 +9,7 @@
 # Receiving the first argument as Instance Name
 INSTANCE_NAME=$1
 
-if [ -z $INSTANCE_NAME ]; then
+if [ -z ${INSTANCE_NAME} ]; then
   echo -e "\e[1;31mThe Instance Name has not been passed\e[0m"
   exit
 fi
@@ -17,7 +17,7 @@ fi
 # describe-image to get the AMI ID
 AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-7-DevOps-Practice" --query 'Images[*].[ImageId]' --output text)
 
-if [ -z $AMI_ID ]; then
+if [ -z ${AMI_ID} ]; then
   echo -e "\e[1;31mUnable to find the Image AMI_ID\e[0m"
   exit
 else
@@ -28,7 +28,7 @@ fi
 
 PRIVATE_IP=$(aws ec2 describe-instances --filters Name=tag:Name,Values=$INSTANCE_NAME --query 'Reservations[*].Instances[*].PrivateIpAddress' --output text)
 
-if [ -z $PRIVATE_IP ]; then
+if [ -z ${PRIVATE_IP} ]; then
   aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --output text --tag-specification "ResourceType=instance,Tags=[{Key=Name,Value=$INSTANCE_NAME}]"
 else
   echo -e "\e[1;33mThe Instance $INSTANCE_NAME is already running\e[0m"
