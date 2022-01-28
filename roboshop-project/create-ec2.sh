69c0cf0276eb50dfda42bc6cdbfb830672455d4b
gauravkr04 <gauravaneja1983@gmail.com>
2022-01-29 04:13:59 +0530

    Creating ec2 instance - getting AMI ID

--- roboshop-project/create-ec2.sh
+++ roboshop-project/create-ec2.sh
@@ -7,10 +7,10 @@
 # IAM for AWS - create users - aws configure
 # describe-image to get the AMI ID
 
-AMI_ID =  aws ec2 describe-images --filters "Name=name,Values=Centos-7-DevOps-Practice" --query 'Images[*].[ImageId]' --output text
+AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-7-DevOps-Practice" --query 'Images[*].[ImageId]' --output text)
 
 if [ -z $AMI_ID ]; then
-  echo "Unable to find the Image AMI_ID"
+  echo -e "\e[1,31mUnable to find the Image AMI_ID-e[0m"
 else
   echo $AMI_ID
 fi
