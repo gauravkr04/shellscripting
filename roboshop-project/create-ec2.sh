769975cf8725cc330f007e8a5c79bc7ea285f8da
gauravkr04 <gauravaneja1983@gmail.com>
2022-01-30 13:38:53 +0530

    Creating ec2 instance - creating Instance - populating temp file for DNS record creation

--- roboshop-project/create-ec2.sh
+++ roboshop-project/create-ec2.sh
@@ -6,6 +6,10 @@
 # aws ec2 describe-instance
 # IAM for AWS - create users - aws configure
 
+# Create a log file
+INS_CREATE_LOG_FILE = /tmp/instance-create.log
+rm -rf $INS_CREATE_LOG_FILE
+
 # Receiving the first argument as Instance Name
 INSTANCE_NAME=$1
 
@@ -33,7 +37,7 @@ if [ -z "${PRIVATE_IP}" ]; then
     echo -e "\e[1;31mSecurity Group allow-all-ports does not exist; hence exiting\e[0m"
     exit
   fi
-  aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --output text --tag-specification "ResourceType=instance,Tags=[{Key=Name,Value=$INSTANCE_NAME}]" "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=$INSTANCE_NAME}]" --instance-market-options "MarketType=spot,SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}" --security-group-ids=${SEC_GRP}
+  aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --output text --tag-specification "ResourceType=instance,Tags=[{Key=Name,Value=$INSTANCE_NAME}]" "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=$INSTANCE_NAME}]" --instance-market-options "MarketType=spot,SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}" --security-group-ids=${SEC_GRP} &>>INS_CREATE_LOG_FILE
 else
   echo -e "\e[1;33mThe Instance $INSTANCE_NAME is already running\e[0m"
 fi
@@ -52,17 +56,18 @@ echo '{
                               "TTL": 300,
                               "ResourceRecords": [{ "Value": "IPADDRESS"}]
        }}]
-}' | sed -e "s/DNSNAME/$INSTANCE_NAME/" -e "s/IPADDRESS/$IPADDRESS/" >/tmp/record.json
+}' | sed -e "s/DNSNAME/$INSTANCE_NAME/" -e "s/IPADDRESS/$IPADDRESS/" >/tmp/record.json &>>INS_CREATE_LOG_FILE
 
 # Retrieve the Hosted Zone ID
 ZONE_ID=$(aws route53 list-hosted-zones --query "HostedZones[*].{name:Name,id:Id}" --output text |grep roboshop.internal | awk '{print$1'} | awk -F / '{print$3}')
+echo $ZONE_ID
 
 if [ -z "${ZONE_ID}" ]; then
   echo "Hosted Zone ID is not defined. Please create a new one in the console"
   exit
 else
   
-aws route53 change-resource-record-sets --hosted-zone-id $ZONE_ID --change-batch-file:///tmp/record.json --output text
+aws route53 change-resource-record-sets --hosted-zone-id $ZONE_ID --change-batch-file:///tmp/record.json --output text &>>INS_CREATE_LOG_FILE
 
 
 
